8f12dd95905c6d95f9162c1df6fc6c8f57ce3a7d
Suraj Acharya <pmjarusuraj@gmail.com>
2026-04-13 19:53:17 +0545

    Implement user profile route with rate limiting and response structure

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jarusuraj/schoolsystem/middlewares"
+	"github.com/jarusuraj/schoolsystem/models"
+)
+
+func Profile(r *gin.Engine) {
+	r.GET("/profile", middlewares.RateLimit(
+		models.RateLimitConfig{
+			Limit:  60.0 / 60.0,
+			Status: "Request Failed",
+			Body:   "API is at capacity, try again later.",
+		}), func(c *gin.Context) {
+		email, _ := c.Get("email")
+		user_id, _ := c.Get("user_id")
+		role, _ := c.Get("role")
+		c.JSON(200, gin.H{"message": "this is the end hold your breath and count 10....", "email": email, "user_id": user_id, "role": role})
+	})
+}
